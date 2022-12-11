8a9da388bff1a7c61dc8ecb595f5bcef01f89e14
Navein Suresh <naveinksuresh@gmail.com>
2022-12-11 00:20:48 -0800

    New Changes

--- tag_testing.py
+++ tag_testing.py
@@ -1,32 +1,4 @@
-# libraries needed for abs graph visualization
-import pytplot
-import numpy as np
-from pytplot import tplot
-from scipy.io import readsav
-from astropy.time import Time
-
-# Single function that takes 'file_path' as an argument
-def visual(file_path):
-
-    # Opening the abs script 30file (binary to readable text)
-    sav_data = readsav(file_path)
-    print(sav_data)
-    a = sav_data["fomstr"]
-
-    start_times = a["START"][0] 
-    stop_times = a["STOP"][0] 
-    print(start_times)
-    print(stop_times)
-    timestamp_values = a["TIMESTAMPS"][0] 
-    print(timestamp_values)
-    fom_values = a["FOM"][0] 
-    print(fom_values)
-    seg_length = a["SEGLENGTHS"][0]
-    print(seg_length)
-    num_segs = a["NSEGS"][0]
-    print(num_segs)
-
-# LIST OF ALL TAGS:
+# LIST OF ALL TAGS: (Check google docs for thorough instructions)
 
     # "VALID"
     # "ERROR"
@@ -34,14 +6,14 @@ def visual(file_path):
     # "SOURCEID"
     # "CYCLESTART"
     # "NUMCYCLES"
-    # "NSEGS"
-    # "START"
-    # "STOP"
-    # "SEGLENGTHS"
-    # "FOM"
-    # "NBUFFS"
+  ### "NSEGS"
+  ### "START"
+  ### "STOP"
+  ### "SEGLENGTHS"
+  ### "FOM"
+  ### "NBUFFS"
     # "MDQ"
-    # "TIMESTAMPS"
+  ### "TIMESTAMPS"
     # "TARGETBUFFS"
     # "FOMAVE"
     # "TARGETRATIO"
@@ -57,3 +29,68 @@ def visual(file_path):
     # "OLDESTAVAILABLEBURSTDATA"
     # "METADATAEVALTIME"
     # "UPLINKFAG"
+
+
+{'fomstr': rec.array([(
+1, 
+'', 
+b'$Revision: 1.10 $', 
+array([b'ABS', b'ABS', b'ABS', b'ABS', b'ABS', b'ABS', b'ABS', b'ABS', b'ABS', b'ABS'], dtype=object), 
+2037232710, 
+25921, 
+10,  
+array([  412,   474,   713,  1041,  1085,  2369,  2402, 11036, 11175, 11258], dtype=int32), 
+array([  451,   497,   728,  1084,  1128,  2401,  2433, 11074, 11204, 11292], dtype=int32), 
+array([40, 24, 16, 44, 44, 33, 32, 39, 30, 35], dtype=int32), 
+array([77.65743 , 74.29307 , 73.19998 , 73.883896, 77.17816 , 73.10112 , 73.76666 , 73.23331 , 73.58275 , 73.226425], dtype=float32), 
+337, 
+array([0., 0., 0., ..., 0., 0., 0.], dtype=float32), 
+array([2037232710, 2037232720, 2037232730, ..., 2037491890, 2037491900, 2037491910], dtype=uint32), 
+1000, 
+101.359, 
+3., 
+12, 
+60, 
+1, 
+1., 
+6, 
+20., 
+0.5, 
+0.5, 
+b'MMS1=38.72% MMS2=40.12% MMS3=41.50% MMS4=42.88%', 
+b'MMS1=2036548960 MMS2=2016028970 MMS3=2016055210 MMS4=2016062270', 
+b'2022-206T16:45:49', 
+0)],
+
+
+dtype=[
+(('valid', 'VALID'), '>i4'), 
+(('error', 'ERROR'), 'O'), 
+('algversion', 'ALGVERSION'), 'O'), (
+('sourceid', 'SOURCEID'), 'O'), 
+(('cyclestart', 'CYCLESTART'), '>i4'), 
+(('numcycles', 'NUMCYCLES'), '>i4'),
+(('nsegs', 'NSEGS'), '>i4'),
+(('start', 'START'), 'O'),
+(('stop', 'STOP'), 'O'),
+(('seglengths', 'SEGLENGTHS'), 'O'),
+(('fom', 'FOM'), 'O'),
+(('nbuffs', 'NBUFFS'), '>i4'),
+(('mdq', 'MDQ'), 'O'),
+(('timestamps', 'TIMESTAMPS'), 'O'),
+(('targetbuffs', 'TARGETBUFFS'), '>i4'),
+(('fomave', 'FOMAVE'), '>f4'),
+(('targetratio', 'TARGETRATIO'), '>f4'),
+(('minsegmentsize', 'MINSEGMENTSIZE'), '>i4'),
+(('maxsegmentsize', 'MAXSEGMENTSIZE'), '>i4'),
+(('pad', 'PAD'), '>i4'),
+(('searchratio', 'SEARCHRATIO'), '>f4'),
+(('fomwindowsize', 'FOMWINDOWSIZE'), '>i4'),
+(('fomslope', 'FOMSLOPE'), '>f4'),
+(('fomskew', 'FOMSKEW'), '>f4'),
+(('fombias', 'FOMBIAS'), '>f4'),
+(('metadatainfo', 'METADATAINFO'), 'O'),
+(('oldestavailableburstdata', 'OLDESTAVAILABLEBURSTDATA'), 'O'),
+(('metadataevaltime', 'METADATAEVALTIME'), 'O'),
+(('uplinkflag', 'UPLINKFLAG'), '>i4')
+])}
